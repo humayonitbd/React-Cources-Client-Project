6648e1a989175f05041ff3e342ab6766b795caff
humayonitbd <humayonforid44@gmail.com>
2022-10-25 16:11:39 +0600

    courses page

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,9 +1,48 @@
 import React from 'react';
 
 const Login = () => {
+    const loginHandler = (e)=>{
+        e.preventDefault();
+        const form = e.target;
+        const email = form.email.value;
+        const password = form.password.value;
+        console.log(email, password)
+    }
+
+
+
+
     return (
-        <div>
-            <h3>login page</h3>
+        <div className='logContainer'>
+            <div className="hero  min-h-screen bg-base-200">
+            <div className="hero-content flex-col">
+                <div className="text-center">
+                <h1 className="text-5xl font-bold mb-4 lg:mb-10">Login now!</h1>
+                </div>
+                <form onSubmit={loginHandler} className="card flex-shrink-0 max-w-sm shadow-2xl bg-base-100">
+                <div className="card-body">
+                    <div className="form-control">
+                    <label className="label">
+                        <span className="label-text">Email</span>
+                    </label>
+                    <input type="email" name='email' placeholder="email" className="input input-bordered lg:w-80" />
+                    </div>
+                    <div className="form-control">
+                    <label className="label">
+                        <span className="label-text">Password</span>
+                    </label>
+                    <input type="password" name='password' placeholder="password" className="input input-bordered lg:w-80" />
+                    <label className="label">
+                        <a href="#s" className="label-text-alt link link-hover">Forgot password?</a>
+                    </label>
+                    </div>
+                    <div className="form-control mt-6">
+                    <button type='submit' className="btn btn-primary">login</button>
+                    </div>
+                </div>
+                </form>
+            </div>
+            </div>
         </div>
     );
 };
--- src/Pages/Register/Register.js
+++ src/Pages/Register/Register.js
@@ -1,9 +1,62 @@
-import React from 'react';
+import React from 'react'; 
+import './Register.css'
 
 const Register = () => {
+
+
+    const registerHandler = (e)=>{
+        e.preventDefault();
+        const form = e.target;
+        const name = form.name.value;
+        const photoUrl = form.photourl.value;
+        const email = form.email.value;
+        const password = form.password.value;
+        console.log(name, photoUrl, email, password)
+    }
+
+
+
+
     return (
-        <div>
-            <h3>register page</h3>
+        <div className='registerContainer'>
+            <div className="hero  min-h-screen bg-base-200">
+            <div className="hero-content flex-col">
+                <div className="text-center">
+                <h1 className="text-5xl font-bold mb-4 lg:mb-10">Register now!</h1>
+                </div>
+                <form onSubmit={registerHandler} className="card flex-shrink-0 max-w-sm shadow-2xl bg-base-100">
+                <div className="card-body">
+                    <div className="form-control">
+                    <label className="label">
+                        <span className="label-text">FullName</span>
+                    </label>
+                    <input type="text" name='name' placeholder="name" className="input input-bordered lg:w-80" />
+                    </div>
+                    <div className="form-control">
+                    <label className="label">
+                        <span className="label-text">PhotoURL</span>
+                    </label>
+                    <input type="text" name='photourl' placeholder="name" className="input input-bordered lg:w-80" />
+                    </div>
+                    <div className="form-control">
+                    <label className="label">
+                        <span className="label-text">Email</span>
+                    </label>
+                    <input type="email" name='email' placeholder="email" className="input input-bordered lg:w-80" />
+                    </div>
+                    <div className="form-control">
+                    <label className="label">
+                        <span className="label-text">Password</span>
+                    </label>
+                    <input type="password" name='password' placeholder="password" className="input input-bordered lg:w-80" />
+                    </div>
+                    <div className="form-control mt-6">
+                    <button type='submit' className="btn btn-primary">Register</button>
+                    </div>
+                </div>
+                </form>
+            </div>
+            </div>
         </div>
     );
 };
